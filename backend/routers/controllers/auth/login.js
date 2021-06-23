3f22767956769b5236f6ea08a439edd4faae5424
Abdalgani2 <abdalgani.alqade@gmail.com>
2021-06-23 22:32:33 +0300

    edit login backend

--- backend/routers/controllers/auth/login.js
+++ backend/routers/controllers/auth/login.js
@@ -1,32 +1,31 @@
 const jwt = require('jsonwebtoken');
 const db = require("./../../../db/db")
-const login =  (req, res) => {
-  const email  = req.body.email;
-  const password =req.body.password;
-    const query = `SELECT * FROM user WHERE email = ? ;`
+const login = (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const query = `SELECT * FROM user WHERE email = ? ;`
   const data = [email];
   db.query(query, data, (err, result) => {
-    console.log("results:",result)
-   if(!result[0]){
+    if (!result[0]) {
       return res.status(404).json("the email dosnt exist");
-   }
-   const confirm =  bcrypt.compare(password, result[0].password);
-			if (confirm) {
-				const payload = {
-					name: result[0].name,
-					password: result[0].password,
-					role: result[0].id,
-				};
+    }
+    const confirm = bcrypt.compare(password, result[0].password);
+    if (confirm) {
+      const payload = {
+        name: result[0].name,
+        password: result[0].password,
+        role: result[0].id,
+      };
 
-				const options = {
-					expiresIn: '1d',
-				};
-				res.status(200).json({token:jwt.sign(payload, process.env.SECRET, options), message:"valid login",user:result[0]});
-			}
-      res.json('The password is not correct').status(403)
-		});	
-  }
+      const options = {
+        expiresIn: '1d',
+      };
+      res.status(200).json({ token: jwt.sign(payload, process.env.SECRET, options), message: "valid login", user: result[0] });
+    }
+    res.json('The password is not correct').status(403)
+  });
+}
 
-  module.exports = {
-    login,
-  };
+module.exports = {
+  login,
+};
