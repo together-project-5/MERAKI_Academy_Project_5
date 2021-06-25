3fb1bcddda00f43e6da882af312e7a735ac6ea5c
Your Name <mostafa.jalamneh17@gmail.com>
2021-06-25 10:28:06 +0300

    solved conflict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,19 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Login from "./components/auth/login/index";
-<<<<<<< HEAD
 import ShowPosts from './components/post'
 import CreatePost from './components/post/postCraete'
+import Register from "./components/register/index";
+
 
 const App = () => {
 	return <>
 	<Route path='main' component={ShowPosts}/>
 	<Route path="/login" component={Login} />
 	<Route path="/createPost" component={CreatePost} />
+  <Route path="/register" component={Register} />
+
 </>
 };
-=======
-import Register from "./components/register/index";
-
-function App() {
-  return (
-    <div className="App">
-      <Route path="/login" component={Login} />
-	  <Route path="/register" component={Register} />
-    </div>
-  );
-}
->>>>>>> 7e3f9a991154998fa5ff098272c52ad53b764d60
 
 export default App;
