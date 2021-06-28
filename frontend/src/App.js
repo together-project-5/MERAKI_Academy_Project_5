47a4fa1321de41527bb1224c198115a3931b6e92
arefallabadi <labadiaref@gmail.com>
2021-06-28 09:36:38 +0300

    done with getPostByType

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { Route } from "react-router-dom";
 import Login from "./components/auth/login/index";
 import CreatePosts from "./components/post/postCreate";
 import Register from "./components/auth/signUp/index";
-import GetPost from "./components/main/post"
+import GetPost from "./components/main/post";
 import { Header } from "./components/header/index";
 import Main from "./components/main/index";
 
@@ -11,18 +11,13 @@ const App = () => {
   return (
     <>
       <Route path="/" component={GetPost} />
-      <Route path='/' component={Header} />
-      <Route path="main" component={ShowPosts} />
+      <Route path="/" component={Header} />
       <Route path="/login" component={Login} />
       <Route path="/createPost" component={CreatePosts} />
-	  <Route path="/register" component={Register} />
-	  <Route path="/Main" component={Main} />
+      <Route path="/register" component={Register} />
+      <Route path="/Main" component={Main} />
     </>
-    
-    
   );
 };
 
 export default App;
-
-
