ff7587b75eb1008c34f647317fb59f46f8ce34df
AhmadAlAraj06 <81652458+AhmadAlAraj06@users.noreply.github.com>
2021-06-30 09:28:25 +0300

    finish buttons list and save user data

--- frontend/src/components/main/postList.js
+++ frontend/src/components/main/postList.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import IconButton from '@material-ui/core/IconButton';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-
-
-    
-const options = [
-  'Report',
-];
+import React from "react";
+import IconButton from "@material-ui/core/IconButton";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 
 const ITEM_HEIGHT = 48;
 
 export default function LongMenu() {
+  const history = useHistory();
+
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
 
@@ -22,6 +21,37 @@ export default function LongMenu() {
 
   const handleClose = () => {
     setAnchorEl(null);
+  };
+
+  const reportPost = () => {
+
+  };
+
+  const editPost = () => {
+    axios
+      .delete(`http://localhost:5000/edit/:id`)
+      .then((res) => {})
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const archivePost = () => {
+    axios
+      .delete(`http://localhost:5000/archive`)
+      .then((res) => {})
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const deletePost = (_IdPost) => {
+    axios
+      .delete(`http://localhost:5000/:id`)
+      .then((res) => {})
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
@@ -43,16 +73,49 @@ export default function LongMenu() {
         PaperProps={{
           style: {
             maxHeight: ITEM_HEIGHT * 4.5,
-            width: '20ch',
+            width: "20ch",
           },
         }}
       >
-        {options.map((option) => (
-          <MenuItem key={option} selected={option === 'Pyxis'} onClick={handleClose}>
-            {option}
-          </MenuItem>
-        ))}
+        <MenuItem
+          onClick={(e) => {
+            e.preventDefault();
+            handleClose();
+            reportPost();
+          }}
+        >
+          Report
+        </MenuItem>
+        <MenuItem
+          onClick={(e) => {
+            e.preventDefault();
+            handleClose();
+            editPost();
+          }}
+        >
+          Edit Post
+        </MenuItem>
+        <MenuItem
+          onClick={(e) => {
+            e.preventDefault();
+            handleClose();
+            archivePost();
+          }}
+        >
+          Archive
+        </MenuItem>
+        <MenuItem
+          onClick={(e) => {
+            e.preventDefault();
+            handleClose();
+            deletePost();
+          }}
+        >
+          Delete
+        </MenuItem>
       </Menu>
     </div>
   );
 }
+
+// onClick={deletePost}
