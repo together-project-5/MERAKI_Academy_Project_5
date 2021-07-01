dffdc2ed8bbba90ce918b8820456e14c9aa6f504
AhmadAlAraj06 <81652458+AhmadAlAraj06@users.noreply.github.com>
2021-07-02 00:24:31 +0300

    finish menu list

--- frontend/src/components/main/postList.js
+++ frontend/src/components/main/postList.js
@@ -13,6 +13,7 @@ export default function LongMenu({ id }) {
   const history = useHistory();
   const [_IdPost, set_IdPost] = useState("");
   const [report, setReport] = useState(0);
+  const [userId, setUserId] = useState("");
 
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
@@ -64,7 +65,71 @@ export default function LongMenu({ id }) {
       });
   };
 
-  return (
+
+
+
+
+
+
+
+
+
+  
+  if (_IdPost === userId) {
+    return (
+      <div>
+        <IconButton
+          aria-label="more"
+          aria-controls="long-menu"
+          aria-haspopup="true"
+          onClick={handleClick}
+        >
+          <MoreVertIcon />
+        </IconButton>
+        <Menu
+          id="long-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={open}
+          onClose={handleClose}
+          PaperProps={{
+            style: {
+              maxHeight: ITEM_HEIGHT * 4.5,
+              width: "20ch",
+            },
+          }}
+        >
+          <MenuItem
+            onClick={(e) => {
+              e.preventDefault();
+              handleClose();
+              editPost(id);
+            }}
+          >
+            Edit Post
+          </MenuItem>
+          <MenuItem
+            onClick={(e) => {
+              e.preventDefault();
+              handleClose();
+              archivePost(id);
+            }}
+          >
+            Archive
+          </MenuItem>
+          <MenuItem
+            onClick={(e) => {
+              e.preventDefault();
+              handleClose();
+              deletePost(id);
+            }}
+          >
+            Delete
+          </MenuItem>
+        </Menu>
+      </div>
+    );
+  } else {
     <div>
       <IconButton
         aria-label="more"
@@ -96,34 +161,170 @@ export default function LongMenu({ id }) {
         >
           Report
         </MenuItem>
-        <MenuItem
-          onClick={(e) => {
-            e.preventDefault();
-            handleClose();
-            editPost(id);
-          }}
-        >
-          Edit Post
-        </MenuItem>
-        <MenuItem
-          onClick={(e) => {
-            e.preventDefault();
-            handleClose();
-            archivePost(id);
-          }}
-        >
-          Archive
-        </MenuItem>
-        <MenuItem
-          onClick={(e) => {
-            e.preventDefault();
-            handleClose();
-            deletePost(id);
-          }}
-        >
-          Delete
-        </MenuItem>
       </Menu>
-    </div>
-  );
+    </div>;
+  }
+
+
+//test
+
+
+
+// if (_IdPost === userId) {
+//   return (
+//     <div>
+//     <IconButton
+//       aria-label="more"
+//       aria-controls="long-menu"
+//       aria-haspopup="true"
+//       onClick={handleClick}
+//     >
+//       <MoreVertIcon />
+//     </IconButton>
+//     <Menu
+//       id="long-menu"
+//       anchorEl={anchorEl}
+//       keepMounted
+//       open={open}
+//       onClose={handleClose}
+//       PaperProps={{
+//         style: {
+//           maxHeight: ITEM_HEIGHT * 4.5,
+//           width: "20ch",
+//         },
+//       }}
+//     >
+//       <MenuItem
+//         onClick={(e) => {
+//           e.preventDefault();
+//           handleClose();
+//           reportPost(id);
+//         }}
+//       >
+//         Report
+//       </MenuItem>
+//     </Menu>
+//   </div>
+//   );
+// } else {
+
+// <div>
+//       <IconButton
+//         aria-label="more"
+//         aria-controls="long-menu"
+//         aria-haspopup="true"
+//         onClick={handleClick}
+//       >
+//         <MoreVertIcon />
+//       </IconButton>
+//       <Menu
+//         id="long-menu"
+//         anchorEl={anchorEl}
+//         keepMounted
+//         open={open}
+//         onClose={handleClose}
+//         PaperProps={{
+//           style: {
+//             maxHeight: ITEM_HEIGHT * 4.5,
+//             width: "20ch",
+//           },
+//         }}
+//       >
+//         <MenuItem
+//           onClick={(e) => {
+//             e.preventDefault();
+//             handleClose();
+//             editPost(id);
+//           }}
+//         >
+//           Edit Post
+//         </MenuItem>
+//         <MenuItem
+//           onClick={(e) => {
+//             e.preventDefault();
+//             handleClose();
+//             archivePost(id);
+//           }}
+//         >
+//           Archive
+//         </MenuItem>
+//         <MenuItem
+//           onClick={(e) => {
+//             e.preventDefault();
+//             handleClose();
+//             deletePost(id);
+//           }}
+//         >
+//           Delete
+//         </MenuItem>
+//       </Menu>
+//     </div>
+  
+// }
+
+//original code
+
+
+  // return (
+  //   <div>
+  //     <IconButton
+  //       aria-label="more"
+  //       aria-controls="long-menu"
+  //       aria-haspopup="true"
+  //       onClick={handleClick}
+  //     >
+  //       <MoreVertIcon />
+  //     </IconButton>
+  //     <Menu
+  //       id="long-menu"
+  //       anchorEl={anchorEl}
+  //       keepMounted
+  //       open={open}
+  //       onClose={handleClose}
+  //       PaperProps={{
+  //         style: {
+  //           maxHeight: ITEM_HEIGHT * 4.5,
+  //           width: "20ch",
+  //         },
+  //       }}
+  //     >
+  //       <MenuItem
+  //         onClick={(e) => {
+  //           e.preventDefault();
+  //           handleClose();
+  //           reportPost(id);
+  //         }}
+  //       >
+  //         Report
+  //       </MenuItem>
+  //       <MenuItem
+  //         onClick={(e) => {
+  //           e.preventDefault();
+  //           handleClose();
+  //           editPost(id);
+  //         }}
+  //       >
+  //         Edit Post
+  //       </MenuItem>
+  //       <MenuItem
+  //         onClick={(e) => {
+  //           e.preventDefault();
+  //           handleClose();
+  //           archivePost(id);
+  //         }}
+  //       >
+  //         Archive
+  //       </MenuItem>
+  //       <MenuItem
+  //         onClick={(e) => {
+  //           e.preventDefault();
+  //           handleClose();
+  //           deletePost(id);
+  //         }}
+  //       >
+  //         Delete
+  //       </MenuItem>
+  //     </Menu>
+  //   </div>
+  // );
 }
