4d573a1b2a42cebec62a8aa52f80df5282b048fc
Your Name <mostafa.jalamneh17@gmail.com>
2021-07-04 09:37:06 +0300

    edit sever.js

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,10 @@ const google = require("./routers/controllers/google/google")
 
 const app = express();
 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(cors());
+
 //routers
 
 //built-in middlewares
