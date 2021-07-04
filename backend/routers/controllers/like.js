ac48261cc3308c319555fdbe117fc87ddfb58928
Abdalgani2 <abdalgani.alqade@gmail.com>
2021-07-04 13:27:04 +0300

    check the user like

--- backend/routers/controllers/like.js
+++ backend/routers/controllers/like.js
@@ -1,8 +1,18 @@
 const createLike =async (req, res) => {
     const query = `INSERT INTO post (userId  , postId) VALUES (?,?)`;
     let { userId, postId } = req.body;
+    const data = [userId,postId ]
     db.query(query, data, (err, result) => {
         if (err) return res.status(400)
         res.status(201).json(result);
       });
 }
+const getLikePost = (req, res) => {
+    const query = `SELECT FROM POST WHERE userId = ? , postId = ?`;
+    let { userId, postId } = req.body;
+    const data = [userId,postId ]
+    db.query(query, data, (err, result) => {
+        if (err) return res.status(400)
+        res.status(201).json(result);
+      });
+  };
