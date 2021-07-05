78fba54c986972884bc2220f6490f51e059d34fd
arefallabadi <labadiaref@gmail.com>
2021-07-05 10:18:25 +0300

    create myInfo and myProfile (COMPONENTS)

---
+++ frontend/src/components/myProfile/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AllPost from "./../main/allPost/index";
+import MyInfo from "./myInfo";
+
+const MyProfile = () => {
+  return (
+    <>
+      <MyInfo />
+      <AllPost />
+    </>
+  );
+};
+export default MyProfile;
