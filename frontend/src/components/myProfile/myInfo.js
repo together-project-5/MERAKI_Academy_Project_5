a000c2806388f47e8b13f1c6157dc8360813eb44
arefallabadi <labadiaref@gmail.com>
2021-07-05 10:59:35 +0300

    add myPost component in myProfile

--- frontend/src/components/myProfile/myInfo.js
+++ frontend/src/components/myProfile/myInfo.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const MyInfo = ()=>{
-    return (
-        <>
-         my info
-        </>
-      );
+import { useDispatch, useSelector } from "react-redux";
+
+const MyInfo = () => {
+  const dispatch = useDispatch();
+//   const [user, setUser] = useState("");
+  const state = useSelector((state) => {
+    return {
+      posts: state.posts.posts,
+      user: state.login.user,
     };
-    export default MyInfo;
+  });
+
+//   useEffect(async() => {
+//     await axios
+//       .get(`http://localhost:5000/user/name/${localStorage.getItem("name")}`)
+//       .then(async(res) => {
+//        await setUser(res.data);
+//         console.log(res.data);
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   }, []);
+
+  return (
+    <>
+      <div>
+          
+        {/* <p>{user[0].name}</p>
+        <p>{user[0].email}</p>
+        <p>{user[0]._IdUser}</p>
+        <img src={user[0].picture} /> */}
+      </div>
+    </>
+  );
+};
+export default MyInfo;
