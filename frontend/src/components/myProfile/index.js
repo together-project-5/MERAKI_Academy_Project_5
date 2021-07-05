9492c79bc410e697de16ce155022f81269258ed9
arefallabadi <labadiaref@gmail.com>
2021-07-05 12:12:07 +0300

    add button on clike push to createPost component

--- frontend/src/components/myProfile/index.js
+++ frontend/src/components/myProfile/index.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+
 import MyPost from "./myPost";
 import MyInfo from "./myInfo";
 
 const MyProfile = () => {
+  const history = useHistory();
+
+    const handleClick = () => {
+        history.push("/createPost");
+      };
+
   return (
     <>
       <MyInfo />
+      <button className="post" onClick={handleClick}>
+        Create Post
+      </button>
       <MyPost />
     </>
   );
