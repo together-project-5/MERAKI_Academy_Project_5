289b743d4d68adb1a2a941b49c51c203fc52b28a
AhmadAlAraj06 <81652458+AhmadAlAraj06@users.noreply.github.com>
2021-07-06 19:20:38 +0300

    edit profile simple style

--- frontend/src/components/user/editProfile.js
+++ frontend/src/components/user/editProfile.js
@@ -1,79 +1,101 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useDispatch } from "react-redux";
-import setUser from "../../reducers/login"
+import setUser from "../../reducers/login";
+import TextField from "@material-ui/core/TextField";
+import editProfile from "./editProfile.css";
 
 const EditProfile = () => {
   const [userData, setUserData] = useState({});
   const dispatch = useDispatch();
-  const [fileInputState, setFileInputState] = useState('')
-  const [previewSource, setPreviewSource] = useState()
+  const [fileInputState, setFileInputState] = useState("");
+  const [previewSource, setPreviewSource] = useState();
 
   const handleChange = (e) => {
     setUserData({ ...userData, [e.target.name]: e.target.value });
-  }
+  };
 
   const handleFileInputChange = (e) => {
     const file = e.target.files[0];
-    previewFile(file)
-
-  }
+    previewFile(file);
+  };
 
   const previewFile = (file) => {
     const reader = new FileReader();
-    reader.readAsDataURL(file)
+    reader.readAsDataURL(file);
     reader.onloadend = () => {
-      setPreviewSource(reader.result)
-    }
+      setPreviewSource(reader.result);
+    };
   };
 
   const handleSubmitFile = (e) => {
     e.preventDefault();
     if (!previewSource) return;
-    uploadImage(previewSource)
+    uploadImage(previewSource);
     // const reader = new FileReader();
     // reader.readAsDataURL(selectedFile);
-  }
+  };
   const uploadImage = async (base64EncodedImage) => {
     try {
-      await fetch(`http://localhost:5000/user/edit/${localStorage.getItem("_IdUser")}`, {
-        method: 'put',
-        body: JSON.stringify({ data: base64EncodedImage, userData: userData }),
-        headers: { 'Content-type': 'application/json' },
-      }).then((result) => {
+      await fetch(
+        `http://localhost:5000/user/edit/${localStorage.getItem("_IdUser")}`,
+        {
+          method: "put",
+          body: JSON.stringify({
+            data: base64EncodedImage,
+            userData: userData,
+          }),
+          headers: { "Content-type": "application/json" },
+        }
+      ).then((result) => {
         console.log("result", result);
-      })
+      });
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
   return (
     <>
-      <div >
-        <form >
-          <input
+      <div>
+        <form>
+          <div className="UserIdPosts">
+          <TextField
+            id="outlined-textarea"
+            label="User Name"
+            multiline
+            variant="outlined"
+            onChange={handleChange}
             name="name"
-            type="text"
-            placeholder=" User Name"
-            onChange={handleChange}
           />
-          <input
+          </div>
+          <div className="typePosts">
+          <TextField
+            id="outlined-textarea"
+            label="Password"
+            multiline
+            variant="outlined"
+            onChange={handleChange}
             name="password"
-            type="text"
-            placeholder=" password"
-            onChange={handleChange}
           />
-          <input type="file" name="image" onChange=
-            {handleFileInputChange} value={fileInputState} />
+          </div>
+          <div className="uploadImg">
+
+          <input
+            type="file"
+            name="image"
+            onChange={handleFileInputChange}
+            value={fileInputState}
+          />
           <button onClick={handleSubmitFile}>Submit</button>
           {previewSource && (
-            <img src={previewSource} alt="chosen"
-              style={{ height: '300px' }} />
+            <img src={previewSource} alt="chosen" style={{ height: "300px" }} />
           )}
+      </div>
+
         </form>
       </div>
     </>
   );
-}
-export default EditProfile
+};
+export default EditProfile;
