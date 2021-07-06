a73f6e87a73e86b418f0165a4eeb39cf0bc3f874
AhmadAlAraj06 <81652458+AhmadAlAraj06@users.noreply.github.com>
2021-07-06 20:37:56 +0300

    edit profile simple style

--- frontend/src/components/myProfile/myInfo.js
+++ frontend/src/components/myProfile/myInfo.js
@@ -10,12 +10,11 @@ const MyInfo = () => {
       <div className="profile-nav">
         <img
           className="profile-pic"
-          src="https://www.attendit.net/images/easyblog_shared/July_2018/7-4-18/b2ap3_large_totw_network_profile_400.jpg"
+          src={user.picture}
         />
         <h1 className="name">{user.name}</h1>
         {/* <p>{user.email}</p> */}
         {/* <p>{user._IdUser}</p> */}
-        <img src={user.picture} />
         <br />
       <Divider />
       <br />
