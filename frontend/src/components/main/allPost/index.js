4932f8dca20b27b94eae385e9ae067c702f3db04
AhmadAlAraj06 <81652458+AhmadAlAraj06@users.noreply.github.com>
2021-07-07 23:41:17 +0300

    change profile pic

--- frontend/src/components/main/allPost/index.js
+++ frontend/src/components/main/allPost/index.js
@@ -58,10 +58,23 @@ const AllPost = () => {
                 <div className="post-div" key={i}>
                   <div className="user-information-list">
                     <div className="user-information">
-                      <img
-                        className="profile-picture"
-                        src="https://www.attendit.net/images/easyblog_shared/July_2018/7-4-18/b2ap3_large_totw_network_profile_400.jpg"
-                      />
+                    <div className="user-pic">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="55"
+                        height="55"
+                        fill="currentColor"
+                        class="bi bi-person-circle"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                        <path
+                          fill-rule="evenodd"
+                          d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+                        />
+                      </svg>
+                    </div>
+
                       <p className="user-post-name">{post.name}</p>
                     </div>
                     <div className="messege-post">
