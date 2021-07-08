bcd16971bfe6f3b9ce6cf0342a9998be66c0f6d7
AhmadAlAraj06 <81652458+AhmadAlAraj06@users.noreply.github.com>
2021-07-08 11:43:32 +0300

    styling 5

--- frontend/src/components/main/allPost/index.js
+++ frontend/src/components/main/allPost/index.js
@@ -10,9 +10,6 @@ import ShowComment from "../comment/show";
 import "./allPost.css";
 import MainPage from "../mainPage";
 import Divider from "@material-ui/core/Divider";
-import IconButton from "@material-ui/core/IconButton";
-import Badge from "@material-ui/core/Badge";
-import MailIcon from "@material-ui/icons/Mail";
 
 const AllPost = () => {
   const dispatch = useDispatch();
@@ -63,11 +60,16 @@ const AllPost = () => {
                         />
                         <p className="user-post-name">{post.name}</p>
                         <div className="user-msg">
-                          <IconButton color="inherit">
-                            <Badge color="secondary">
-                              <MailIcon />
-                            </Badge>
-                          </IconButton>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="20"
+                            height="20"
+                            fill="currentColor"
+                            class="bi bi-chat-fill"
+                            viewBox="0 0 16 16"
+                          >
+                            <path d="M8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6-.097 1.016-.417 2.13-.771 2.966-.079.186.074.394.273.362 2.256-.37 3.597-.938 4.18-1.234A9.06 9.06 0 0 0 8 15z" />
+                          </svg>
                         </div>
                         <div className="post-tp">
                           <svg
