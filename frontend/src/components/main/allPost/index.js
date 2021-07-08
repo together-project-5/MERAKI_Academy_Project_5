6d83761e1914796c44990a6c2bd3b653557a317b
AhmadAlAraj06 <81652458+AhmadAlAraj06@users.noreply.github.com>
2021-07-08 11:32:09 +0300

    styling 3

--- frontend/src/components/main/allPost/index.js
+++ frontend/src/components/main/allPost/index.js
@@ -6,10 +6,13 @@ import Comment from "../comment/index";
 import Like from "../like/index";
 import Save from "../save/index";
 import MenuItem from "../postList";
-import ShowComment from "../comment/show";  
+import ShowComment from "../comment/show";
 import "./allPost.css";
 import MainPage from "../mainPage";
 import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import Badge from "@material-ui/core/Badge";
+import MailIcon from "@material-ui/icons/Mail";
 
 const AllPost = () => {
   const dispatch = useDispatch();
@@ -59,7 +62,25 @@ const AllPost = () => {
                           src="https://www.attendit.net/images/easyblog_shared/July_2018/7-4-18/b2ap3_large_totw_network_profile_400.jpg"
                         />
                         <p className="user-post-name">{post.name}</p>
+                        <div className="user-msg">
+                          <IconButton color="inherit">
+                            <Badge color="secondary">
+                              <MailIcon />
+                            </Badge>
+                          </IconButton>
+                        </div>
+                        <div className="post-tp">
+
+                        <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-clock-fill" viewBox="0 0 16 16">
+  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
+</svg>
+</div>
+
+                        <div className="post-td">
+                          <h5>3 min ago</h5>
+                        </div>
                       </div>
+
                       <MenuItem
                         id={post._IdPost}
                         onClick={() => {
@@ -72,19 +93,16 @@ const AllPost = () => {
                       />
                     </div>
                     <div className="post-information">
-                    <p className="post-title">{post.title}</p>
-                        <p className="post-description">{post.description}</p>
+                      <p className="post-title">{post.title}</p>
+                      <p className="post-description">{post.description}</p>
                     </div>
 
                     <div className="post-information">
-
                       <img className="post-image" src={post.url} />
-                      <div className="post-information-2">
-                  
-                      </div>
+                      <div className="post-information-2"></div>
                     </div>
-                    <br/>
-      <Divider />
+                    <br />
+                    <Divider />
 
                     <div className="post-reaction">
                       <div className="post-reaction-2">
