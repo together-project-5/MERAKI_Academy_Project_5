5edabde33b10c222067104cc8fa673e985269a40
AhmadAlAraj06 <81652458+AhmadAlAraj06@users.noreply.github.com>
2021-07-10 20:28:25 +0300

    pull

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,30 +13,47 @@ import EditProfile from "./components/user/editProfile";
 import Upload from "./components/upload/upload";
 import EditPost from "./components/editPost/index";
 import Erorr from "./components/error";
-import {Switch} from "react-router-dom"
-
-
+import { Switch } from "react-router-dom";
+import { useSelector } from "react-redux";
 const App = () => {
-  return (
-    <>
-      <Route path="/" component={Header} />
-      <Route exact path="/" component={Main} />
-    
-      <Switch>
-      <Route exact path="/" component={GetPost} />
-      <Route path="/profile" component={MyProfile} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-      <Route path="/archive" component={Archive} />
-      <Route path="/favorite" component={GetFavorites} />
-      <Route path="/post" component={postList} />
-      <Route path="/createPost" component={Upload} />
-      <Route path="/editProfile" component={EditProfile} />
-      <Route path="/editPost" component={EditPost} />
-      <Route component={Erorr} />
-      </Switch>
-    </>
-  );
+
+  const token = useSelector((state) => {
+    return {
+      token: state.login.token,
+      user: state.login.user,
+    };
+  });
+console.log(token.user);
+  if (token.user) {
+    return (
+      <>
+        <Route path="/" component={Header} />
+        <Route exact path="/" component={Main} />
+
+        <Switch>
+          <Route exact path="/" component={GetPost} />
+          <Route path="/profile" component={MyProfile} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/archive" component={Archive} />
+          <Route path="/favorite" component={GetFavorites} />
+          <Route path="/post" component={postList} />
+          <Route path="/createPost" component={Upload} />
+          <Route path="/editProfile" component={EditProfile} />
+          <Route path="/editPost" component={EditPost} />
+          <Route component={Erorr} />
+        </Switch>
+      </>
+    );
+  } else {
+    return (
+      <>
+          <Route path="/login" component={Login} />
+
+
+      </>
+    )
+  }
 };
 
 export default App;
