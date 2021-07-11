1696474acfb4c7f0f332af996a59903a6663c1d5
Your Name <mostafa.jalamneh17@gmail.com>
2021-07-11 15:57:19 +0300

    import Features component in togther

--- frontend/src/components/together/index.js
+++ frontend/src/components/together/index.js
@@ -1,18 +1,28 @@
 import React from "react";
 import "./style.css"
+import Features from "./features";
 
 const Together = () => {
 
     return (
         <>
-            <body>
-                <div className="firstBox">
-                    <h1 class="firstHeader">
-                        <span class="mainText">Together</span>
-                        <span class="subText">A world of communication and learning</span>
-                    </h1>
-                </div>
-            </body>
+            <section>
+                <body>
+                    <div className="firstBox">
+                        <h1 class="firstHeader">
+                            <span class="mainText">Together</span>
+                            <span class="subText">A world of communication and learning</span>
+                        </h1>
+                    </div>
+                </body>
+            </section>
+
+            <section>
+                <Features />
+            </section>
+
+
+
         </>
     )
 
