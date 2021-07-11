34b2e43d8fda0d7f2304d484e7c11f6eabb95161
Abdalgani2 <abdalgani.alqade@gmail.com>
2021-07-11 13:04:27 +0300

    creat use effect to get all post the same title

--- frontend/src/components/search/index.js
+++ frontend/src/components/search/index.js
@@ -1,25 +1,35 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
- 
+
 
 import axios from "axios";
 
 const SearchTitl = () => {
+  const [posts, setPosts] = useState([]);
   const state = useSelector((state) => {
     return {
       title: state.searchTitle.search,
-      
+
     };
   });
-  console.log("statetitle",state.title);
-  return(
-  <>
-    {console.log("title111")}
-    <div className ="aaa" ><h1>aaaaa</h1></div>
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/post/title/${state.title}`)
+      .then((res) => {
+        console.log(res.data)
+        setPosts(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <>
+      <div className="aaa" ><h1>aaaaa</h1></div>
     </>
-  
+
   )
-  
+
 
 }
 
