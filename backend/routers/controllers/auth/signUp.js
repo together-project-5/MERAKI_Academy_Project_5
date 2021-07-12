2de6790bd9c518a0553264f1374244b601d815db
AhmadAlAraj06 <81652458+AhmadAlAraj06@users.noreply.github.com>
2021-07-12 22:52:27 +0300

    required users

--- backend/routers/controllers/auth/signUp.js
+++ backend/routers/controllers/auth/signUp.js
@@ -4,15 +4,28 @@ const { request } = require("express");
 const salt = Number(process.env.SALT);
 
 const createUser = async (req, res) => {
-  const query = `INSERT INTO user (name ,email,password,username) VALUES (?,?,?,?)`;
   let { name, email, password, username } = req.body;
-  password = await bcrypt.hash(password, salt);
-  email = await email.toLowerCase();
-  const data = [name, email, password, username];
-  db.query(query, data, (err, result) => {
-    if (err) res.send("Please fill all text");
-    res.status(201).json(result);
-  });
+  if (!name) {
+    res.status(400).send("please fill name text");
+  }
+  if (!email) {
+    res.status(400).send("please fill email text");
+  }
+  if (!password) {
+    res.status(400).send("please fill password text");
+  }
+  if (!username) {
+    res.status(400).send("please fill username text");
+  } else {
+    const query = `INSERT INTO user (name ,email,password,username) VALUES (?,?,?,?)`;
+    password = await bcrypt.hash(password, salt);
+    email = await email.toLowerCase();
+    const data = [name, email, password, username];
+    db.query(query, data, (err, result) => {
+      if (err) res.send("Please fill all text");
+      res.status(201).json(result);
+    });
+  }
 };
 
 module.exports = {
