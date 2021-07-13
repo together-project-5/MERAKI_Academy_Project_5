83a1352fdc0b9e0798bdbf147ea8da84dccd6cb6
Abdalgani2 <abdalgani.alqade@gmail.com>
2021-07-13 14:57:15 +0300

    add function edit post

--- frontend/src/components/editPost/index.js
+++ frontend/src/components/editPost/index.js
@@ -1,18 +1,26 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
 
 const EditPost = () => {
+  const [description, setDescription] = useState("");
+  const [title, setTitle] = useState("");
   const state = useSelector((state) => {
     return {
       post: state.getPost.getPost,
 
     };
   });
-  
+  const edit = () => {
+    const handleChangeTitle = (e) => {
+      setTitle(e.target.value);
+    };
+    
   return (
     <>
       <div>
-        <input type="text" placeholder="Title" />
+        <input type="text" placeholder="Title" 
+         onChange={handleChangeTitle}/>
         <input type="text" placeholder="description" />
         <button>Submit</button>
       </div>
