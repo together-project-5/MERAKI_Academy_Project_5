50e936bd37bbe819a56e11ba4331ed7a39733c9d
arefallabadi <labadiaref@gmail.com>
2021-07-13 22:32:36 +0300

    delete number for notifictions

--- frontend/src/components/header/menuList.js
+++ frontend/src/components/header/menuList.js
@@ -206,7 +206,7 @@ body {
                 aria-label="show 17 new notifications"
                 color="inherit"
               >
-                <Badge badgeContent={17} color="secondary">
+                <Badge color="secondary">
                   <NotificationsIcon />
                 </Badge>
               </IconButton>
