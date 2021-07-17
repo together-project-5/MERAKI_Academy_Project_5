be95cee0cbe43aa66b4a7e7911efac30335831ac
AhmadAlAraj06 <81652458+AhmadAlAraj06@users.noreply.github.com>
2021-07-17 15:14:57 +0300

    css

--- client/src/components/editPost/index.js
+++ client/src/components/editPost/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
+import TextField from "@material-ui/core/TextField";
 
 const EditPost = () => {
   const [description, setDescription] = useState("");
@@ -8,39 +9,60 @@ const EditPost = () => {
   const state = useSelector((state) => {
     return {
       post: state.getPost.getPost,
-
     };
   });
   const handleChangeTitle = (e) => {
-      setTitle(e.target.value);
-    };
-    const handleChangeDescription = (e) => {
-      setDescription(e.target.value);
-    };
+    setTitle(e.target.value);
+  };
+  const handleChangeDescription = (e) => {
+    setDescription(e.target.value);
+  };
   const edit = () => {
     axios
-      .put(`${process.env.REACT_APP_BACKEND_SERVER}/post/edit/${state.post[0]._IdPost}`,
+      .put(
+        `${process.env.REACT_APP_BACKEND_SERVER}/post/edit/${state.post[0]._IdPost}`,
         {
           title: title,
-          description: description
-        })
+          description: description,
+        }
+      )
       .then((res) => {
-        console.log(res.data)
-
+        console.log(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
-    
-  }
+  };
   return (
     <>
       <div>
-        <input type="text" placeholder="Title" 
-         onChange={handleChangeTitle}/>
-        <input type="text" placeholder="description"
-         onChange={handleChangeDescription} />
-        <button onClick={edit}>Submit</button>
+        <div className="TitlePost">
+          <TextField
+            id="outlined-textarea"
+            label="Title"
+            multiline
+            variant="outlined"
+            onChange={handleChangeTitle}
+            name="title"
+            type="text"
+          />
+        </div>
+        {/* <input type="text" placeholder="Title" 
+         onChange={handleChangeTitle}/> */}
+        <div className="TitlePost">
+          <TextField
+            id="outlined-textarea"
+            label="description"
+            multiline
+            variant="outlined"
+            onChange={handleChangeDescription}
+            name="title"
+            type="text"
+          />
+        </div>
+        {/* <input type="text" placeholder="description"
+         onChange={handleChangeDescription} /> */}
+        <button className="postSubmits" onClick={edit}>Submit</button>
       </div>
     </>
   );
